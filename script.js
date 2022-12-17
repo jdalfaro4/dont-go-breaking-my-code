892041c2dae2bc2852ce8dc0e479ef8515b306f4
jdalfaro4 <jdalfaro4@gmail.com>
2022-12-17 10:14:10 -0600

    adding variables to assign characters and password length function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var upppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+var numericCharacters = "1234567890"
+var specialCharacters = "$#!&%^*()"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,11 +10,25 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
 }
+
+function generatePassword() {
+  // prompt user to get criteria and store results
+  
+  var passwordLength =  window.prompt('What will be your password length? Must be between 8 to 128 characters.');
+  passwordLength = parseInt(passwordLength);
+
+  if(passwordLength >= 8 && passwordLength <= 128) {
+    console.log("Correct number chosen")
+    randomPassword(passwordLength)
+  } else {
+    alert("Please enter password length between 8-128")
+    generatePassword();
+  }
+}
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
